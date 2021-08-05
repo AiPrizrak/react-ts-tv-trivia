d53a0158b43d5b1103b1fd427fee67c5cd863e89
AiPrizrak <nnwarts@gmail.com>
2021-08-05 05:03:04 +0300

    added key

--- src/API.ts
+++ src/API.ts
@@ -25,7 +25,7 @@ export const API_KEY = `d2f2eb83bd3651e2b293c650c3d27ab2`;
 export type QuestionsState = Question & { answers: string[] };
 
 export const fetchQuizQuestions = async (): Promise<QuestionsState[]> => {
-  const endpoint = `https://api.themoviedb.org/tv/top_rated/1?api_key=${API_KEY}`;
+  const endpoint = `https://api.themoviedb.org/tv/top_rated?api_key=${API_KEY}`;
   const data = await (await fetch(endpoint)).json();
   return data.results.map((result: Result) => (
     <Question>{question:result.name, hint: result.overview}
